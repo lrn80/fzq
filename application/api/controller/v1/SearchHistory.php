8ba91a4343bbde34967fc8f166bd36ed6e60556b
李若宁 <liruoning@liruoningdeMacBook-Pro.local>
2021-04-19 19:59:59 +0800

    feat: 获取搜索历史列表

---
+++ application/api/controller/v1/SearchHistory.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace app\api\controller\v1;
+
+
+use app\api\controller\BaseController;
+use app\api\service\Token;
+use app\api\service\SearchHistory as SearchHistoryService;
+class SearchHistory extends BaseController
+{
+    /**
+     * 获取搜索历史列表
+     * @return \think\response\Json
+     * @throws \app\exception\TokenException
+     * @throws \think\Exception
+     */
+    public function getSearchHistoryList()
+    {
+        $uid = Token::getCurrentTokenVar('id');
+        return json(SearchHistoryService::getSearchHistoryList($uid));
+    }
+}
