dc1b62a942c3f9eccb113c33abff5b19e2257fe0
李若宁 <liruoning@liruoningdeMacBook-Pro.local>
2021-04-20 11:58:20 +0800

    feat: add news_id

--- application/api/service/Collect.php
+++ application/api/service/Collect.php
@@ -11,7 +11,7 @@ class Collect
     public static function userCollect($uid, $news_id)
     {
         $category_model = new CategoryModel();
-        $category_info = $category_model->where(['uid' => $uid, 'id' => $news_id])->find();
+        $category_info = $category_model->where(['id' => $news_id])->find();
         if (empty($category_info)) {
             return false;
         }
@@ -24,7 +24,7 @@ class Collect
 
         $res = (new UserCollectNews())->insert($data);
         if (!$res) {
-            Log::error(__METHOD__ . "user collect news fail uid:{$uid} cid:{$cid}");
+            Log::error(__METHOD__ . "user collect news fail uid:{$uid} cid:{$news_id}");
             return false;
         }
 
